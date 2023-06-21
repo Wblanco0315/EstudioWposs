dabe20c45c4a3b11bb9dd9ef13a8d73bb711b123
Wblanco0315 <wowblanco071@gmail.com>
2023-06-21 15:59:17 -0500

    atributos clase persona

--- src/main/java/com/example/EstudioWposs/models/Persona.java
+++ src/main/java/com/example/EstudioWposs/models/Persona.java
@@ -1,4 +1,20 @@
 package com.example.EstudioWposs.models;
 
-public class Persona {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+@Entity
+@Data
+@Table(name = "persona")
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_persona;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
 }
