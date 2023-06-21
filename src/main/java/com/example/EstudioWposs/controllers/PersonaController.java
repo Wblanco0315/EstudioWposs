e091e149d8af09a12bfeb44141546dedd1170eeb
JuanDanielG <jdgaleanon@ufpso.edu.co>
2023-06-21 18:40:54 -0500

    controlador

--- src/main/java/com/example/EstudioWposs/controllers/PersonaController.java
+++ src/main/java/com/example/EstudioWposs/controllers/PersonaController.java
@@ -1,11 +1,13 @@
 package com.example.EstudioWposs.controllers;
 
+import com.example.EstudioWposs.models.Persona;
 import com.example.EstudioWposs.service.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -14,11 +16,25 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
+    //Funcion encargada de imprimir la base de datos y mostrar el index
     @GetMapping("/")
     public String inicio(Model model){
         var personas = personaService.listarPersona();
         model.addAttribute("personas", personas);
         return "index";
+    }
+   //Funcion encargada de redirigir a la ventana de agregar datos
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+
+        return "modificar";
+    }
+
+    //Funcion encargada de guardar la información en la base de datos
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
     }
 
 }
