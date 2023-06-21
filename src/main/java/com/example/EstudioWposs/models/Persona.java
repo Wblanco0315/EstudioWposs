3507f90f21570af30796d4741f963081a448414c
Xxarhabia <msarabiacarrascal@gmail.com>
2023-06-21 18:19:30 -0500

    modificar vista

--- src/main/java/com/example/EstudioWposs/models/Persona.java
+++ src/main/java/com/example/EstudioWposs/models/Persona.java
@@ -1,6 +1,7 @@
 package com.example.EstudioWposs.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,9 +13,18 @@ public class Persona implements Serializable {
     private static final long serialVersionUID=1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @NotEmpty
     private long id_persona;
+
+    @NotEmpty
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
+    @NotEmpty
     private String email;
+
     private String telefono;
 }
