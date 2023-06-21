4c69a0c8db15d85721f3416f0e8355e490ac0ca7
Wblanco0315 <wowblanco071@gmail.com>
2023-06-21 16:16:09 -0500

    atributos clase persona

--- src/main/java/com/example/EstudioWposs/service/PersonaServiceImpl.java
+++ src/main/java/com/example/EstudioWposs/service/PersonaServiceImpl.java
@@ -1,29 +1,34 @@
 package com.example.EstudioWposs.service;
 
+import com.example.EstudioWposs.dao.PersonaDao;
 import com.example.EstudioWposs.models.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PersonaServiceImpl implements PersonaService{
+
+    @Autowired
+    private PersonaDao personaDao;
     @Override
     public List<Persona> listarPersona() {
-        return null;
+        return (List<Persona>) personaDao.findAll();
     }
 
     @Override
     public void guardar(Persona persona) {
-
+        personaDao.save(persona);
     }
 
     @Override
     public void eliminar(Persona persona) {
-
+        personaDao.delete(persona);
     }
 
     @Override
     public Persona buscarPersona(Persona persona) {
-        return null;
+        return personaDao.findById(persona.getId_persona()).orElse(null);
     }
 }
