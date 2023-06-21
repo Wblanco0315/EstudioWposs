d984aa46ec7fdc57a192c8044b374da96ffd9552
Wblanco0315 <wowblanco071@gmail.com>
2023-06-21 15:09:08 -0500

    subir archivo

---
+++ src/main/java/com/example/EstudioWposs/EstudioWpossApplication.java
@@ -0,0 +1,13 @@
+package com.example.EstudioWposs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioWpossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioWpossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EstudioWposs/EstudioWpossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EstudioWposs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioWpossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
